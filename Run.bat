8b6ad3e11c4b3ca000911c63e7905c80d4ab183d
ronknight <webmaster@pinoyitsolution.com>
2019-02-27 10:50:47 -0800

    Initial commit

---
+++ Run.bat
@@ -0,0 +1,9 @@
+:: Author: Ronknight
+:: Date: 12/27/18
+:: License: MIT
+:: Tags: Batch, Python, py, bat
+
+@echo off
+:: location of your python file
+python "C:\Plus\export-woocommerce-products-wordpress\export-woocommerce-products-wordpress.py" %*
+pause
